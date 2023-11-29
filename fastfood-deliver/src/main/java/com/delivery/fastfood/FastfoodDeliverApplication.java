1e984d3bb5129bc6ba0252489ca2bfeea653c875
azimochilov <azimochilov226@gmail.com>
2023-11-29 16:10:47 +0500

    project created!

---
+++ fastfood-deliver/src/main/java/com/delivery/fastfood/FastfoodDeliverApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.fastfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfoodDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfoodDeliverApplication.class, args);
+	}
+
+}
---
+++ fastfood-deliver/src/main/java/com/delivery/fastfood/domain/entities/Role.java
@@ -0,0 +1,7 @@
+package com.delivery.fastfood.domain.entities;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+@Entity
+public class Role implements Serializable {
+}
