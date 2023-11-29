da1ebdf21cf7e72a1624dd41d26bc941f5820f4e
azimochilov <azimochilov226@gmail.com>
2023-11-29 16:13:14 +0500

    role created!

--- fastfood-deliver/src/main/java/com/delivery/fastfood/domain/entities/Role.java
+++ fastfood-deliver/src/main/java/com/delivery/fastfood/domain/entities/Role.java
@@ -1,7 +1,21 @@
 package com.delivery.fastfood.domain.entities;
 
+import com.sun.istack.NotNull;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.io.Serializable;
 @Entity
 public class Role implements Serializable {
+    @Id
+    @NotNull
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
